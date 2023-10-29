86566476b55225c5cf3d76e15c19c01eda69f62c
paju3125 <prajvalgandhi483@gmail.com>
2023-10-29 12:40:54 +0530

    added sidebar and option to upload image

--- streamlitApp.py
+++ streamlitApp.py
@@ -2,57 +2,39 @@ import cv2
 import numpy as np
 import streamlit as st
 
-# Load an image
-image = cv2.imread('charlie.jpeg')
+# Create a Streamlit app
+st.title('Image Transformations')
+
+# Add an image upload option
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+if uploaded_image is not None:
+    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
+else:
+    st.write("Please upload an image.")
+
+# Transformation options in the sidebar
+transformation = st.sidebar.radio("Select Transformation", ("Original", "Translation", "Rotation", "Scaling", "Shearing"))
 
 # Set the desired width and height for the output images
 output_width = 800
 output_height = 600
 
-# Resize the original image to match the output dimensions
-image = cv2.resize(image, (output_width, output_height))
-
-# Define the transformation matrix for each operation
-
-# Translation
-def apply_translation(image):
-    translation_matrix = np.float32([[1, 0, 100], [0, 1, 50]])  # Translate 100 pixels right and 50 pixels down
-    return cv2.warpAffine(image, translation_matrix, (output_width, output_height))
-
-# Rotation
-def apply_rotation(image):
-    rotation_angle = 30  # Rotate 30 degrees
-    rotation_matrix = cv2.getRotationMatrix2D((output_width / 2, output_height / 2), rotation_angle, 1)
-    return cv2.warpAffine(image, rotation_matrix, (output_width, output_height))
-
-# Scaling
-def apply_scaling(image):
-    scaling_matrix = np.float32([[1.2, 0, 0], [0, 0.8, 0]])  # Scale horizontally by 1.2 times and vertically by 0.8 times
-    return cv2.warpAffine(image, scaling_matrix, (output_width, output_height))
-
-# Shearing
-def apply_shearing(image):
-    shearing_matrix = np.float32([[1, 0.2, 0], [0.2, 1, 0]])  # Apply shear transformation
-    return cv2.warpAffine(image, shearing_matrix, (output_width, output_height))
-
-# Create a Streamlit app
-st.title('Image Transformations')
-
-# Display the original image
-st.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), caption='Original Image', use_column_width=True)
-
-# Transformation options
-selected_transformation = st.radio("Select Transformation", ("Translation", "Rotation", "Scaling", "Shearing"))
-
 # Apply and display the selected transformation
-if selected_transformation == "Translation":
-    transformed_image = apply_translation(image)
-elif selected_transformation == "Rotation":
-    transformed_image = apply_rotation(image)
-elif selected_transformation == "Scaling":
-    transformed_image = apply_scaling(image)
-else:
-    transformed_image = apply_shearing(image)
-
-# Display the transformed image
-st.image(cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB), caption=f'{selected_transformation} Image', use_column_width=True)
+if transformation == "Original" and uploaded_image is not None:
+    st.image(image, caption='Original Image', use_column_width=True, channels="BGR")
+elif uploaded_image is not None:
+    if transformation == "Translation":
+        translation_matrix = np.float32([[1, 0, 100], [0, 1, 50]])  # Translate 100 pixels right and 50 pixels down
+        transformed_image = cv2.warpAffine(image, translation_matrix, (output_width, output_height))
+    elif transformation == "Rotation":
+        rotation_angle = 30  # Rotate 30 degrees
+        rotation_matrix = cv2.getRotationMatrix2D((output_width / 2, output_height / 2), rotation_angle, 1)
+        transformed_image = cv2.warpAffine(image, rotation_matrix, (output_width, output_height))
+    elif transformation == "Scaling":
+        scaling_matrix = np.float32([[1.2, 0, 0], [0, 0.8, 0]])  # Scale horizontally by 1.2 times and vertically by 0.8 times
+        transformed_image = cv2.warpAffine(image, scaling_matrix, (output_width, output_height))
+    elif transformation == "Shearing":
+        shearing_matrix = np.float32([[1, 0.2, 0], [0.2, 1, 0]])  # Apply shear transformation
+        transformed_image = cv2.warpAffine(image, shearing_matrix, (output_width, output_height))
+    
+    st.image(transformed_image, caption=f'{transformation} Image', use_column_width=True, channels="BGR")
